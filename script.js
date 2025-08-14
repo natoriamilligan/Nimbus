0aa0ea87d4cb2b1539a5f967fe1f1e18f704a7e3
Natoria Milligan <natoriaray@gmail.com>
2025-08-14 16:32:06 -0500

    added listener to toggle navigation bar on mobile devices

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const navToggle = document.getElementById("nav-toggle");
+    const navLinks = document.getElementById("nav-links");
+
+    navToggle.addEventListener("click", () => {
+        event.preventDefault();
+        navLinks.classList.toggle("active");
+        
+    });
+})
