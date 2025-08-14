bbe91e287b57cbb5d79d158158dbf1829fe5830f
Natoria Milligan <natoriaray@gmail.com>
2025-08-14 16:57:29 -0500

    made sure that the header size stays the same when nav bars are toggled

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navToggle = document.getElementById("nav-toggle");
     const navLinks = document.getElementById("nav-links");
+    const headerBar = document.getElementsByTagName("header")[0];
 
     navToggle.addEventListener("click", () => {
         event.preventDefault();
         navLinks.classList.toggle("active");
-        
+        headerBar.classList.toggle("active");
     });
 })
