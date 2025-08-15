483740ef5678e077e47c97ec731679692503c33a
Natoria Milligan <natoriaray@gmail.com>
2025-08-15 16:06:13 -0500

    deleted header variable and event listener. no longer needed

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navToggle = document.getElementById("nav-toggle");
     const navLinks = document.getElementById("nav-links");
-    const headerBar = document.getElementsByTagName("header")[0];
 
     navToggle.addEventListener("click", () => {
         event.preventDefault();
         navLinks.classList.toggle("active");
-        headerBar.classList.toggle("active");
     });
 })
