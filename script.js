ab54671bbc2b751989298b8a5f4057cc3b1beff5
Natoria Milligan <natoriaray@gmail.com>
2025-08-21 13:53:32 -0500

    added toggle functionality to body tag for no scrolling when nav button is clicked on mobile versions

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const navToggle = document.getElementById("nav-toggle");
     const navLinks = document.getElementById("nav-links");
+    const bodyTag = document.getElementsByTagName("body")[0];
 
     navToggle.addEventListener("click", () => {
         event.preventDefault();
         navLinks.classList.toggle("active");
+        bodyTag.classList.toggle("no-scroll");
     });
 })
