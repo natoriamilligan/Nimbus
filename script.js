32db6ab786444068373345cb36164a8c2e3611be
Natoria Milligan <natoriaray@gmail.com>
2025-08-22 10:01:54 -0500

    toggle functionality for teams sub menu

--- script.js
+++ script.js
@@ -2,10 +2,17 @@ document.addEventListener("DOMContentLoaded", () => {
     const navToggle = document.getElementById("nav-toggle");
     const navLinks = document.getElementById("nav-links");
     const bodyTag = document.getElementsByTagName("body")[0];
+    const navTeamsList = document.getElementById("nav-teams-list");
+    const navTeamsTitle = document.getElementById("nav-teams-title");
 
     navToggle.addEventListener("click", () => {
         event.preventDefault();
         navLinks.classList.toggle("active");
         bodyTag.classList.toggle("no-scroll");
+        navTeamsList.classList.remove("teams-active")
     });
+
+    navTeamsTitle.addEventListener("click", () => {
+        navTeamsList.classList.toggle("teams-active");
+    })
 })
