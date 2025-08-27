dd9b337568f1e55a2cf4245e12a75394bb478caa
Natoria Milligan <natoriaray@gmail.com>
2025-08-27 09:50:29 -0500

    added event listeners to priing buttons (monthly vs. yearly)

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
+//Event listensers
+
 document.addEventListener("DOMContentLoaded", () => {
     const navToggle = document.getElementById("nav-toggle");
     const navLinks = document.getElementById("nav-links");
     const bodyTag = document.getElementsByTagName("body")[0];
     const navTeamsList = document.getElementById("nav-teams-list");
     const navTeamsTitle = document.getElementById("nav-teams-title");
+    const monthlyBtn = document.getElementsByClassName("monthly-pricing-btn")[0];
+    const yearlyBtn = document.getElementsByClassName("yearly-pricing-btn")[0];
+
 
     navToggle.addEventListener("click", () => {
         event.preventDefault();
@@ -15,4 +20,15 @@ document.addEventListener("DOMContentLoaded", () => {
     navTeamsTitle.addEventListener("click", () => {
         navTeamsList.classList.toggle("teams-active");
     })
+
+    yearlyBtn.addEventListener("click", () => {
+        yearlyBtn.style.backgroundColor = "#3fd6f0";
+        monthlyBtn.style.backgroundColor = "rgb(217, 217, 217)";
+    })
+
+    monthlyBtn.addEventListener("click", () => {
+        yearlyBtn.style.backgroundColor = "rgb(217, 217, 217)";
+        monthlyBtn.style.backgroundColor = "#3fd6f0";
+    })
+
 })
